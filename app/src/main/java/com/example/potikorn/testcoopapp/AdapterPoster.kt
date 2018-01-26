104b3ef605dda12941e620e71193984c4df2c044
karnzaaNaja <karn939.n@gmail.com>
2018-01-26 13:42:18 +0700

    Commit Todey 26/1/2018

--- app/src/main/java/com/example/potikorn/testcoopapp/AdapterPoster.kt
+++
@@ -1,7 +0,0 @@
-package com.example.potikorn.testcoopapp
-
-/**
- * Created by karn on 1/26/2018.
- */
-class AdapterPoster {
-}
---
+++ app/src/main/java/com/example/potikorn/testcoopapp/adapter/AdapterPoster.kt
@@ -0,0 +1,4 @@
+package com.example.potikorn.testcoopapp.adapter
+class AdapterPoster {
+
+}
