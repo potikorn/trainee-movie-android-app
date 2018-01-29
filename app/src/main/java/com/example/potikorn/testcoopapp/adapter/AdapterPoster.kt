19c47be2a491b4e19c1f78e049684d1718ea6fa5
Fluke <flukebevil@gmail.com>
2018-01-29 11:17:56 +0700

    change adapter holder and detailMovie class

--- app/src/main/java/com/example/potikorn/testcoopapp/adapter/AdapterPoster.kt
+++ app/src/main/java/com/example/potikorn/testcoopapp/adapter/AdapterPoster.kt
@@ -6,18 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.example.potikorn.testcoopapp.R
+import com.example.potikorn.testcoopapp.holder.MovieViewHolder
 import com.example.potikorn.testcoopapp.models.Movie
 import com.example.potikorn.testcoopapp.network.BaseUrl
 import kotlinx.android.synthetic.main.list_item_movie.view.*
 
 class AdapterPoster(private var movies: List<Movie>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    var callback: MovieListListener? = null
-    interface MovieListListener {
-        fun onClick(movie: Movie)
-    }
-    fun setOnClickCallback(callBack: MovieListListener) {
-        this.callback = callBack
-    }
+
     fun setItem(items: List<Movie>) {
         movies = items
         notifyDataSetChanged()
@@ -31,16 +26,6 @@ class AdapterPoster(private var movies: List<Movie>) : RecyclerView.Adapter<Recy
     override fun getItemViewType(position: Int): Int = R.layout.list_item_movie
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        return (holder as MovieViewHolder).onBandit(movies[position])
-    }
-    inner class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun onBandit(movie: Movie) {
-            itemView.title.text = movie.title
-            itemView.rating.text = movie.vote_average.toString()
-            itemView.ratingBar.rating = movie.vote_average?.toFloat()!!.div(3)
-            Glide.with(itemView.context).load(BaseUrl.baseUrlImageMovie+movie.poster).into(itemView.TV)
-            itemView.movies_layout.setOnClickListener {
-                callback?.onClick(movie)
-            } }
+        return (holder as MovieViewHolder).onBind(movies[position])
     }
 }
