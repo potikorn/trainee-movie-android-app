c5043a2eef49e7b9e9026529fd46ed22d122ccb4
Fluke <flukebevil@gmail.com>
2018-01-29 15:02:03 +0700

    fixed show detail (tv , movie)

--- app/src/main/java/com/example/potikorn/testcoopapp/holder/TelevisionViewHolder.kt
+++ app/src/main/java/com/example/potikorn/testcoopapp/holder/TelevisionViewHolder.kt
@@ -1,8 +1,11 @@
 package com.example.potikorn.testcoopapp.holder
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.bumptech.glide.Glide
+import com.example.potikorn.testcoopapp.DetailsMovie
+import com.example.potikorn.testcoopapp.DetailsTelevision
 import com.example.potikorn.testcoopapp.models.Television
 import com.example.potikorn.testcoopapp.network.BaseUrl
 import kotlinx.android.synthetic.main.list_item_movie.view.*
@@ -14,8 +17,8 @@ class TelevisionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         itemView.rating.text = television.vote_average
         itemView.ratingBar.rating = television.vote_average.toFloat().div(3)
         Glide.with(itemView.context).load(BaseUrl.baseUrlImageMovie+television.poster_path).into(itemView.TV)
-//        itemView.movies_layout.setOnClickListener {
-//            itemView.context.startActivity(Intent(itemView.context , DetailsMovie::class.java).putExtra(DetailsMovie.Key, movie))
-//    }
+        itemView.movies_layout.setOnClickListener {
+            itemView.context.startActivity(Intent(itemView.context , DetailsTelevision::class.java).putExtra(DetailsMovie.Key, television))
+    }
 }
 }
