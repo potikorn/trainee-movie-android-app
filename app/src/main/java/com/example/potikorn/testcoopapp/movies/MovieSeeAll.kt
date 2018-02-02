4896b510def471f05ef525f34fb3cd9ded8ed099
karnzaaNaja <karn939.n@gmail.com>
2018-02-02 13:51:16 +0700

    test commit

---
+++ app/src/main/java/com/example/potikorn/testcoopapp/movies/MovieSeeAll.kt
@@ -0,0 +1,42 @@
+package com.example.potikorn.testcoopapp.movies
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.example.potikorn.testcoopapp.R
+import com.example.potikorn.testcoopapp.contracter.MovieFragmentContractor
+import com.example.potikorn.testcoopapp.models.movie.Movie
+import com.example.potikorn.testcoopapp.models.movie.MovieType
+import com.example.potikorn.testcoopapp.movies.adapter.AdapterDetails
+import com.example.potikorn.testcoopapp.movies.presenter.MovieFragmentPresenter
+import kotlinx.android.synthetic.main.fragment_theme.*
+
+class MovieSeeAll : AppCompatActivity(), MovieFragmentContractor.View {
+
+    private val movieAdapterSeeAll: AdapterDetails by lazy { AdapterDetails(listOf()) }
+    private var presenter: MovieFragmentContractor.Presenter? = MovieFragmentPresenter(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(R.layout.activity_details_type_movie)
+        presenter?.callPopularData()
+        televisionList?.layoutManager = LinearLayoutManager(this)
+        televisionList.adapter = movieAdapterSeeAll
+    }
+
+    override fun callbackGenres(arrMovie: List<MovieType>?) {
+
+    }
+
+    override fun callBackPopularData(arrMovie: List<Movie>?) {
+        Log.e("test","test"+arrMovie)
+        arrMovie?.let { it->movieAdapterSeeAll.setItem(it) }
+
+    }
+
+    override fun callTopRateData(arrMovie: List<Movie>?) {
+
+    }
+
+
+}
